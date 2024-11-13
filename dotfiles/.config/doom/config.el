4189b3ee9525d2bef72c23533eb6ce4865e62023
Billy Batista <bootlegbilly@protonmail.ch>
2024-11-13 16:33:01 -0500

    add staticcheck and go vet to LSP

--- dotfiles/.config/doom/config.el
+++ dotfiles/.config/doom/config.el
@@ -311,3 +311,22 @@
 (provide 'slint-mode)
 
 ;;; slint-mode.el ends here
+
+(require 'lsp-mode)
+(require 'go-mode)
+
+(add-hook 'go-mode-hook 'lsp-deferred)
+(add-hook 'go-mode-hook 'subword-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook (lambda ()
+                          (setq tab-width 4)
+                          (flycheck-add-next-checker 'lsp 'go-vet)
+                          (flycheck-add-next-checker 'lsp 'go-staticcheck)))
+
+(lsp-register-custom-settings
+ '(("gopls.completeUnimported" t t)
+   ("gopls.staticcheck" t t)))
+
+(after! lsp-mode
+  (setq lsp-diagnostics-provider :flycheck))
