c78e56af58e77571aa6dd3c1fdece84b4810cb07
Billy Batista <bootlegbilly@protonmail.ch>
2025-01-14 09:25:26 -0500

    feat(config): add copilot-chat package and update vterm-shell to fish
    ai all the things

--- dotfiles/.config/doom/packages.el
+++ dotfiles/.config/doom/packages.el
@@ -50,3 +50,6 @@
                                         ;(unpin! t)
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+
+(package! copilot-chat
+  :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el" "dist")))
